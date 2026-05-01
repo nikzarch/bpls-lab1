85ea8c8859b0df2b925644366916c61f41e939cb
nikzarch <razerr468@gmail.com>
2026-05-01 19:31:30 +0300

    rabbit config

--- src/main/java/com/example/labpay/config/JmsConfig.java
+++ src/main/java/com/example/labpay/config/JmsConfig.java
@@ -1,10 +1,27 @@
 package com.example.labpay.config;
 
 
+import jakarta.jms.ConnectionFactory;
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableJms
 public class JmsConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new JmsConnectionFactory(
+                "amqp://admin:admin@localhost:5672"
+        );
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory cf) {
+        return new JmsTemplate(cf);
+    }
+
 }
